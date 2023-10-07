899bb876d954b14035848c168ca874c7cc91e4c0
Aleksey Pavlyutenkov <pavlyutenkovaleksey@yandex.ru>
2023-10-07 22:09:50 +0500

    init project koff

---
+++ koff/main.js
@@ -0,0 +1,26 @@
+import 'normalize.css';
+import './style.scss';
+
+import { Navigation, Thumbs } from 'swiper/modules';
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+new Swiper(".product__slider-main", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".product__arrow_next",
+    prevEl: ".product__arrow_prev",
+  },
+  modules: [Navigation, Thumbs],
+  thumbs: {
+    swiper: swiperThumbnails,
+  },
+});
